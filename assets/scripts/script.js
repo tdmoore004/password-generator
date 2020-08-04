1dba60edcb6dfb81e6fa7119f24e261dc9aa54e3
Tanner Moore <tdmoore004@gmail.com>
2020-08-03 21:35:28 -0600

    Created psuedo code file and added function for generating password.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,42 @@
+// Array's for possible password characters
+let characterOptions = [[" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"], 
+["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], 
+["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"], 
+["A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]]
+
+// Function for generating password
+function generatePassword() {
+
+  // Set length of password
+  let numberOfCharacters = prompt("How many characters would you like your password to contain?");
+
+  // Alerts if password is too short or too long
+  if (numberOfCharacters < 8)
+    alert("Password must be at least 8 characters.");
+  else if (numberOfCharacters > 128)
+    alert("Password can not be more then 128 characters.");
+
+  // Choosing characters to include in password
+  else {
+    let includeSpecial = confirm("Click OK to use special characters.");
+    let includeNumeric = confirm("Click OK to use numeric characters.");
+    let includeLowerCase = confirm("Click OK to use lowercase characters.");
+    let includeUpperCase = confirm("Click OK to use uppercase characters.");
+  }
+
+  // For loop choosing password characters
+  if ((numberOfCharacters >= 8) && (numberOfCharacters <= 128)) {
+    for (let i = 0; i < numberOfCharacters ; i++) {
+      
+      // Randomly choosing character option
+      let passwordOption = Math.floor(Math.random() * (characterOptions.length));
+      let passwordCharacter = Math.floor(Math.random() * (characterOptions[passwordOption].length));
+      console.log(characterOptions[passwordOption][passwordCharacter])
+      console.log(password = password + characterOptions[passwordOption][passwordCharacter])
+    }
+  }
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -8,7 +47,11 @@ function writePassword() {
 
   passwordText.value = password;
 
+  console.log(passwordText)
+  console.log(passwordText.value)
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
---
+++ psuedo.md
@@ -0,0 +1,13 @@
+# Password Generator
+
+## Get user input on what to include in password.
+    Ask the user how many characters they want in their password (8 to 128)
+    Ask if they want to use special characters.
+    Ask if they want to use numeric characters.
+    Ask if they want to use lower case letters and upper case letters.
+
+## Randomly select each character of the password based on user input.
+    Select each character of the password based on what the user chose to include.
+    Repeat for each character up to the number of characters the user selected.
+
+## Return generated password to the user.
